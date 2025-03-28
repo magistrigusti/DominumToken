3a0c459b5dce5bf56ff1f8b4cc639e1780599223
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-28 22:16:31 +0300

    0.1

--- wrappers/Allodium.compile.ts
+++ wrappers/Allodium.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/allodium.tact',
+    target: 'contracts/AllodiumJetton.tact',
     options: {
         debug: true,
     },
